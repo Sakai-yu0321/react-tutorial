28067be5c5d7bc9e7de94fd3f4305d6288498dd8
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-06-17 21:29:30 +0900

    クラスコンポーネントに書き換え

--- src/List.js
+++ src/List.js
@@ -1,23 +1,13 @@
-// export const List = (props) => {
-//   const title = props.title;
-//   return (
-//     <div>
-//       <h4>{ title }</h4>
-//       <div>リストです</div>
-//     </div>
-//   )
-// }
+import React from "react"
 
-// ↑は↓のように書くこともできる。
-// propsを省略し
-// オブジェクトのキーであるタイトルを関数の引数に指定して呼び出す
-// 短く簡潔に書けるからよき！
-
-export const List = ({title}) => {
-  return (
-    <div>
-      <h4>{ title }</h4>
-      <div>リストです</div>
-    </div>
-  )
+export class List extends React.Component {
+  render() {
+    const { title } = this.props;
+    return (
+      <div>
+        <h4>{ title }</h4>
+        <div>リストです</div>
+      </div>
+    )
+  }
 }
