fa8179e4eef077e447eb957781ca779bea38b0ac
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-06-18 10:56:40 +0900

    参考演算子による条件分岐とmapメソッドによる配列の繰り返し表示

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
+//useStateという関数をreactから読み込み
 import { useState } from 'react';
+//ListコンポーネントをList.jsから読み込み
 import { List } from "./List";
+//FormコンポーネントをForm.jsから読み込み
+import { Form } from "./Form";
 
 function App() {
-  const [description, setDescription] = useState('クリック前の表示');
-  
-  const changeDescription = () => {
-    setDescription('クリック後の表示です');
-  }
+  // tabのstateを定義
+  const [tab, setTab] = useState('list');
 
   return (
     <div>
-      { description }
-      <List title="取扱言語一覧"/>
-      <button onClick={changeDescription}>ボタン</button>
+      <header>
+        <ul>
+          {/*リストがクリックされたら、state2をlistに変更*/}
+          <li onClick={() => setTab('list')}>リスト</li>
+          {/*フォームがクリックされたら、state2をformに変更*/}
+          <li onClick={() => setTab('form')}>フォーム</li>
+        </ul>
+      </header>
+      <hr/>
+      {/*state2がlistなら、Listコンポーネントを表示。そうでないなら、Formコンポーネントを表示*/}
+      {
+        tab === 'list' ? <List /> : <Form />
+      }
     </div>
   );
 }
 
+//Appコンポーネントを書き出し
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,23 +1,19 @@
-// export const List = (props) => {
-//   const title = props.title;
-//   return (
-//     <div>
-//       <h4>{ title }</h4>
-//       <div>リストです</div>
-//     </div>
-//   )
-// }
-
-// ↑は↓のように書くこともできる。
-// propsを省略し
-// オブジェクトのキーであるタイトルを関数の引数に指定して呼び出す
-// 短く簡潔に書けるからよき！
-
-export const List = ({title}) => {
+const LANGUAGES = [
+  'JavaScript',
+  'C++',
+  'Ruby',
+  'Java',
+  'PHP',
+  'Go'
+];
+export const List = () => {
   return (
     <div>
-      <h4>{ title }</h4>
-      <div>リストです</div>
+      {
+        LANGUAGES.map((lang, index) => {
+          return <div key={index}>{lang}</div>
+        })
+      }
     </div>
   )
 }
