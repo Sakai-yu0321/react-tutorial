15fc084e2391692cf5e2688de4f76a3c5dcc25ae
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-06-27 15:04:26 +0900

    テーマを変更する関数をcontextを用いて作成

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-//styled-componentsを読み込み
-import styled from 'styled-components';
 //useEffectとuseStateという関数をreactから読み込み
 import { useState } from 'react';
 //ListコンポーネントをList.jsから読み込み
 import { List } from "./List";
 //FormコンポーネントをForm.jsから読み込み
 import { Form } from "./Form";
+import styled from 'styled-components';
 //LANGUAGESコンポーネントをconst/languages.jsから読み込み
 import { getLanguages } from "./const/languages";
 //higher-order-componentを読み込み
 import { withLoading } from './hoc/withLoading';
+import { Header } from './Header';
 
-const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  padding: 24px 64px 0;
-  border-bottom: 1px solid #E0E0E0;
-`
-
-const HeaderUl = styled.ul`
-  display: flex;
-  margin: 0;
-  padding: 0;
-`
-
-const HeaderLi = styled.li`
-  list-style: none;
-  padding: 4px 12px;
-  cursor: pointer;
-  border-bottom: ${props => props.focused ? '2px solid #F44336' :'none' };
-`
 
 function App({ data }) {
   //tabのstateを定義
@@ -47,14 +28,7 @@ function App({ data }) {
 
   return (
     <div>
-      <Header>
-        <HeaderUl>
-          {/*リストがクリックされたら、tabのstateをlistに変更*/}
-          <HeaderLi focused={tab === 'list'} onClick={() => setTab('list')}>リスト</HeaderLi>
-          {/*フォームがクリックされたら、tabのstateをformに変更*/}
-          <HeaderLi focused={tab === 'form'} onClick={() => setTab('form')}>フォーム</HeaderLi>
-        </HeaderUl>
-      </Header>
+      <Header tab={tab} setTab={setTab}/>
       {/*tabのstateがlistなら、Listコンポーネントを表示。そうでないなら、Formコンポーネントを表示
          Listコンポーネントにlangsのstate(言語情報の配列)を渡す
          Formコンポーネントに関数addLangを呼び出すプロパティonAddLangを渡す*/}
